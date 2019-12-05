66ceb8b9a2a99b8e7e397a5e0f4529f41ecab0e3
Anastasia Polozova <polozova.anastasi@gmail.com>
2019-12-05 16:26:54 +0200

    delete unnecessary

--- Land-seo/assets/js/index.js
+++
@@ -1,9 +0,0 @@
-import '@babel/polyfill';
-
-import $ from 'jquery';
-window.$ = $;
-
-// import 'bootstrap';
-
-import './app.js';
-import './jquery.sumoselect.js';
--- Mail-group/assets/js/index.js
+++
@@ -1,8 +0,0 @@
-import '@babel/polyfill';
-
-import $ from 'jquery';
-window.$ = $;
-
-import 'bootstrap';
-
-import './app.js';
